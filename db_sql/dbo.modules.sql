77e918a79acb9a543b3c90374984e635babf03ff
BrunoSantos256 <2201739@my.ipleiria.pt>
2022-11-24 17:16:01 +0000

    criar base de dados e alteração no ficherio schema

---
+++ db_sql/dbo.modules.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[modules] (
+    [Id]          INT           NOT NULL,
+    [name]        NVARCHAR (50) NULL,
+    [creation_dt] DATETIME2 (7) NULL,
+    [parent]      INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_parent_app] FOREIGN KEY ([parent]) REFERENCES [dbo].[applications] ([Id])
+);
+
+
---
+++ db_sql/dbo.subscriptions.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[subscriptions] (
+    [Id]          INT            NOT NULL,
+    [name]        NVARCHAR (50)  NULL,
+    [creation_dt] DATETIME2 (7)  NULL,
+    [event]       NVARCHAR (MAX) NULL,
+    [endpoint]    NVARCHAR (50)  NULL,
+    [parent]      INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_parent_module_sub] FOREIGN KEY ([parent]) REFERENCES [dbo].[modules] ([Id])
+);
+
