bd5037a939462f7467b054a69372179141597ea8
BrunoSantos256 <2201739@my.ipleiria.pt>
2022-12-06 10:35:09 +0000

    database

---
+++ SOMIOD/App_Data/dbo.SOMIOD.sql
@@ -0,0 +1,36 @@
+﻿DROP TABLE subscriptions,data,modules,applications;
+CREATE TABLE [dbo].[applications] (
+    [Id]           INT           NOT NULL,
+    [name]         NVARCHAR (50) NULL,
+    [creationd_dt] DATETIME2 (7) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC) 
+);
+CREATE TABLE [dbo].[modules] (
+    [Id]          INT           NOT NULL,
+    [name]        NVARCHAR (50) NULL,
+    [creation_dt] DATETIME2 (7) NULL,
+    [parent]      INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_parent_app] FOREIGN KEY ([parent]) REFERENCES [dbo].[applications] ([Id])
+);
+
+CREATE TABLE [dbo].[data] (
+    [Id]          INT            NOT NULL,
+    [content]     NVARCHAR (MAX) NULL,
+    [creation_dt] DATETIME2 (7)  NULL,
+    [parent]      INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_parent_module_data] FOREIGN KEY ([parent]) REFERENCES [dbo].[modules] ([Id])
+);
+
+CREATE TABLE [dbo].[subscriptions] (
+    [Id]          INT            NOT NULL,
+    [name]        NVARCHAR (50)  NULL,
+    [creation_dt] DATETIME2 (7)  NULL,
+    [event]       NVARCHAR (MAX) NULL,
+    [endpoint]    NVARCHAR (50)  NULL,
+    [parent]      INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_parent_module_sub] FOREIGN KEY ([parent]) REFERENCES [dbo].[modules] ([Id])
+);
+
--- db_sql/dbo.applications.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[applications] (
-    [Id]           INT           NOT NULL,
-    [name]         NVARCHAR (50) NULL,
-    [creationd_dt] DATETIME2 (7) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
-
