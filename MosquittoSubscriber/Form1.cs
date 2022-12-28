f2e7de22b63488c4c752163df70e4190385889a7
BrunoSantos256 <2201739@my.ipleiria.pt>
2022-12-28 14:09:10 +0000

    mosquitto

---
+++ MosquittoSubscriber/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MosquittoSubscriber
+{
+    public partial class Form1 : Form
+    {
+        MqttClient mClient = new MqttClient("127.0.0.1");
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            String status = "";
+            MessageBox.Show("Received = " + Encoding.UTF8.GetString(e.Message));
+            status = Encoding.UTF8.GetString(e.Message);
+
+            
+
+            
+            if (status == "on")
+            {
+                pictureBox1.BeginInvoke((MethodInvoker)delegate { pictureBox1.Hide(); });
+                pictureBox2.BeginInvoke((MethodInvoker)delegate { pictureBox2.Show(); });
+            }
+            if (status == "off")
+            {
+                pictureBox1.BeginInvoke((MethodInvoker)delegate { pictureBox1.Show(); });
+                pictureBox2.BeginInvoke((MethodInvoker)delegate { pictureBox2.Hide(); });
+            }
+            
+        }
+
+
+        private void a(object sender, EventArgs e)
+        {
+            mClient.Connect(Guid.NewGuid().ToString());
+            if (!mClient.IsConnected)
+            {
+                MessageBox.Show("Error connecting to lightbulb...");
+                return;
+            }
+            byte[] qosLevels = {MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE};
+            string[] mStrTopicsInfo = {"light_bulb"};
+
+            mClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+
+            pictureBox1.Show();
+            pictureBox2.Hide();
+
+
+            mClient.Subscribe(mStrTopicsInfo, qosLevels);
+
+        }
+
+        
+
+    }
+}
--- MosquittoSubscriber/Properties/AssemblyInfo.cs
+++ MosquittoSubscriber/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MosquittoPublisher")]
+[assembly: AssemblyTitle("MosquittoSubscriber")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("MosquittoPublisher")]
+[assembly: AssemblyProduct("MosquittoSubscriber")]
 [assembly: AssemblyCopyright("Copyright © HP 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("55d1fdc4-280d-488c-a628-b7594a43b010")]
+[assembly: Guid("fc3dbe24-2453-4400-8949-bccaeab8d7a7")]
 
 // Version information for an assembly consists of the following four values:
 //
