04616de603e08a794b268d4bdf00b67e45a58790
BrunoSantos256 <2201739@my.ipleiria.pt>
2022-12-30 10:38:23 +0000

    subscriber & publisher

--- MosquittoPublisher/Form1.Designer.cs
+++ MosquittoPublisher/Form1.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -54,17 +62,103 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(56, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose a Application";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 275);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Command Module Name:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Choose a Module/Channel";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(56, 148);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.Click += new System.EventHandler(this.comboBox2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(52, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Command Name:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(56, 353);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(165, 22);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(816, 606);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +166,14 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- MosquittoPublisher/Form1.cs
+++ MosquittoPublisher/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,65 +26,178 @@ namespace MosquittoPublisher
 
     public partial class Form1 : Form
     {
-        MqttClient mClient = new MqttClient("127.0.0.1");
+        
         private static readonly HttpClient client = new HttpClient();
         public Form1()
         {
             InitializeComponent();
             
         }
+
        async private void Form1_Load(object sender, EventArgs e)
         {
-            mClient.Connect(Guid.NewGuid().ToString());
-            if (!mClient.IsConnected)
-            {
-                MessageBox.Show("Error connecting to lightbulb...");
-                return;
-            }
 
-            //GET PARA O DROPDOWN
+
+            //POST DO MODULE LIGHT_COMMAND
+
+            var values_module = new Dictionary<string, string>
+              {
+                  { "res_type", "module" },
+                  { "name", "light_command" }
+              };
+
+            var content_module = new StringContent(
+                                                  JsonConvert.SerializeObject(values_module),
+                                                   System.Text.Encoding.UTF8,
+                                                   "application/json"
+                                                   );
+
+            var response_module = await client.PostAsync("http://localhost:53818/api/somiod/lightning", content_module);
+
+            var responseString_module = await response_module.Content.ReadAsStringAsync();
+
+            MessageBox.Show(responseString_module);
+
+
+            ///////////////////////////////////////////
+        }
+
+       async private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            if(comboBox1.SelectedItem!=null)
+            {
+                
+
+            }
+            else
+            {
+                MessageBox.Show("First Choose a Application!");
+            }
+            */
+
+            //POST DO DATA ON
+            var values_data = new Dictionary<string, string>
+              {
+                  { "res_type", "data" },
+                  { "content", "on" }
+              };
+
+            var content_data = new StringContent(
+                                                  JsonConvert.SerializeObject(values_data),
+                                                   System.Text.Encoding.UTF8,
+                                                   "application/json"
+                                                   );
+
+            var response_data = await client.PostAsync("http://localhost:53818/api/somiod/lightning/light_bulb", content_data);
+
+            var responseString_data = await response_data.Content.ReadAsStringAsync();
+
+            //MessageBox.Show(responseString_module);
+
+        }
+
+       async private void button2_Click(object sender, EventArgs e)
+        {
+            /*
+            if (comboBox1.SelectedItem != null)
+            {
+                if (textBox1.Text!=null)
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Write the name of the Module!");
+                }
+                
+                
+            }
+            else
+            {
+                MessageBox.Show("First Choose a Application!");
+            }
+            */
+
+            //POST DO DATA OFF
+            var values_data = new Dictionary<string, string>
+              {
+                  { "res_type", "data" },
+                  { "content", "off" }
+              };
+
+            var content_data = new StringContent(
+                                                  JsonConvert.SerializeObject(values_data),
+                                                   System.Text.Encoding.UTF8,
+                                                   "application/json"
+                                                   );
+
+            var response_data = await client.PostAsync("http://localhost:53818/api/somiod/lightning/light_bulb", content_data);
+
+            var responseString_data = await response_data.Content.ReadAsStringAsync();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       async private void comboBox1_Click(object sender, EventArgs e)
+        {
+            //GET PARA O DROPDOWN DOS APPS
 
             var response_GET = await client.GetAsync("http://localhost:53818/api/somiod/applications");
             var responseString_GET = await response_GET.Content.ReadAsStringAsync();
 
             List<string> app_names = new List<string>();
-            
+
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.LoadXml(responseString_GET);
 
             XmlNodeList nodes = xmlDoc.SelectNodes("//*[local-name() = 'Name']");
-            
-            
+
+
             foreach (XmlNode node in nodes)
             {
                 app_names.Add(node.InnerText);
-                MessageBox.Show(node.InnerText);
-               
+
+
             }
-            
-            ///////////////////////////////////////////
+            comboBox1.Items.Clear();
+            foreach (String name in app_names)
+            {
+                comboBox1.Items.Add(name);
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void comboBox2_Click(object sender, EventArgs e)
         {
-            if (mClient.IsConnected)
-            {
-              
-                MessageBox.Show("aaa");
-                mClient.Publish("light_bulb", Encoding.UTF8.GetBytes("on"));
-            }
 
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (mClient.IsConnected)
-            {
-
-                MessageBox.Show("bbb");
-                mClient.Publish("light_bulb", Encoding.UTF8.GetBytes("off"));
-            }
         }
     }
 }
